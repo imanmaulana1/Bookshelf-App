25f5f259ddc8e6b93f381c296e01f2e28e9b0301
Iman Maulana <maulanaiman902@gmail.com>
2024-04-24 15:20:05 +0700

    feat: responsive sidebar section

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,10 @@
-const navLinks = document.querySelector('.nav-links');
-let linkItem = '';
+function getLinkItems() {
+  const linkItems = document.querySelectorAll('.link-item');
 
+  return linkItems;
+}
+
+let linkItem = '';
 const navSidebar = [
   {
     icon: 'ri-home-6-line',
@@ -8,22 +12,42 @@ const navSidebar = [
   },
   {
     icon: 'ri-book-open-line',
-    name: 'Selesai Dibaca',
+    name: 'Read',
   },
   {
     icon: 'ri-book-line',
-    name: 'Belum Dibaca',
+    name: 'Unread',
   },
 ];
 
-navSidebar.map((item) => {
-  linkItem += `<li class="link-item">
-        <div class="item-name">
-            <i class="${item.icon}"></i>
-            <h3>${item.name}</h3>
-        </div>
-        <span class="item-length">2</span>
-    </li>`;
+navSidebar.map((item, idx) => {
+  linkItem += `<li class="link-item ${idx == 0 ? 'active' : ''}">
+                <div class="item-name">
+                    <i class="${item.icon}"></i>
+                    <h3>${item.name}</h3>
+                </div>
+                <span class="item-length">2</span>
+                </li>`;
 });
 
+const navLinks = document.querySelector('.nav-links');
 navLinks.innerHTML = linkItem;
+
+const linkItems = getLinkItems();
+linkItems.forEach((item) => {
+  item.addEventListener('click', function () {
+    const items = getLinkItems();
+    items.forEach((item) => {
+      item.classList.remove('active');
+    });
+
+    this.classList.add('active');
+  });
+});
+
+const collapseBtn = document.querySelector('.collapse');
+collapseBtn.addEventListener('click', () => {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.classList.toggle('show');
+  sidebar.style.transition = 'all .4s ease';
+});
